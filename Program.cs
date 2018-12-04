0153f60cf919ed975ee9cfe847506904ad026ffe
Damola Omotosho <damola.omotosho@gmail.com>
2018-12-03 18:47:59 -0800

    Testing wercker workflow

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace chap1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Dee!");
         }
     }
 }
